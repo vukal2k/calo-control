bd4e48230b60f2ac5a938e7f8579b62d0e6dee8b
duc.tranvan <duc.tranvan@petronas.com>
2021-05-23 23:42:21 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <h1>This app will update something.</h1>
+    <>
+      <h1>This app will update something.</h1>
+      <img src="https://scontent-hkg4-1.xx.fbcdn.net/v/t1.6435-9/185984622_1783752848471759_6499596683020666644_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=S18tTgMurY0AX9koPC4&_nc_ht=scontent-hkg4-1.xx&oh=58be9d944a1205c9cf9a2f7df58cde1d&oe=60D19069"
+        style={{ width: '100%', height: '100%' }}
+      />
+    </>
   );
 }
 
